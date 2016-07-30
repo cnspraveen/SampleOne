42dd6b4bc654a0436fcd357d4d4ced92a680ca85
cnspraveen <cns.praveen@gmail.com>
2016-07-30 23:56:58 +0530

    Update NiceClass.java

--- src/NicePack/NiceClass.java
+++ src/NicePack/NiceClass.java
@@ -5,6 +5,7 @@ public class NiceClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Nice CLass.....");
+		System.out.println("Nice Class updated from Github.....");
 
 	}
 
